1cca1c29d42225f4f879b0850f0ea5e7afd2713c
CeciliaSwe <cedergren.cecilia@gmail.com>
2021-09-20 09:46:22 +0000

    Add jshint extension and clean up minor errors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 6 */
+
 //Declares variables for displaying question and options
 let quizContainer = document.getElementById("question-display");
 let resultsContainer = document.getElementById("score-display");
@@ -20,7 +22,7 @@ let posC = 0;
 let posTotal = 0;
 
 //Wait for DOM to load, the shuffle quiz
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   shuffle(quizCatA);
   shuffle(quizCatB);
   shuffle(quizCatC);
@@ -28,85 +30,78 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 // Uses event listner to initialize the quiz with assigning a random value, category and question
-function clickQuestion (event) {
+function clickQuestion(event) {
   let selectDiv = document.getElementsByClassName('category');
   let randomCategory = Math.floor(Math.random() * selectDiv.length);
-  
-  /*console.log(randomCategory, selectDiv[randomCategory]);
-  console.log(randomCategory);
-  console.log('Received the ' + event.type + " event!");
-  console.log('"this" currently refers to', this.id);
-  console.log('You clicked the button!\n');*/
-    
-    questionValue();
-    lightCategory(randomCategory);   
-    displayQ(randomCategory);
+  questionValue();
+  lightCategory(randomCategory);
+  displayQ(randomCategory);
 }
-  
+
 let questionButton = document.getElementById('button-start');
-questionButton.addEventListener('click', clickQuestion); // left clicks
+questionButton.addEventListener('click', clickQuestion);
 
 //Randomised a value from the array and displays in the value section
 function questionValue() {
-    let values = [10, 20, 50, 100];
-    let randomValue = Math.floor(Math.random() * values.length);
-    console.log(values[randomValue]);
-    document.getElementById("value-display").innerHTML = values[randomValue];
+  let values = [10, 20, 50, 100];
+  let randomValue = Math.floor(Math.random() * values.length);
+  console.log(values[randomValue]);
+  document.getElementById("value-display").innerHTML = values[randomValue];
 }
 
 //Changes background color for the randomised category
 function lightCategory(randomCategory) {
-    if (randomCategory === 0) {
-        document.getElementById("c-1").style.backgroundColor = "orange";
-        document.getElementById("c-1").style.color = "blue";
-    } else if (randomCategory === 1) {
-        document.getElementById("c-2").style.backgroundColor = "orange";
-        document.getElementById("c-2").style.color = "blue";
-    } else if (randomCategory === 2) {
-        document.getElementById("c-3").style.backgroundColor = "orange";
-        document.getElementById("c-3").style.color = "blue";
-    }
-    
+  if (randomCategory === 0) {
+    document.getElementById("c-1").style.backgroundColor = "orange";
+    document.getElementById("c-1").style.color = "blue";
+  } else if (randomCategory === 1) {
+    document.getElementById("c-2").style.backgroundColor = "orange";
+    document.getElementById("c-2").style.color = "blue";
+  } else if (randomCategory === 2) {
+    document.getElementById("c-3").style.backgroundColor = "orange";
+    document.getElementById("c-3").style.color = "blue";
+  }
+
 }
 
-//Filters out questions from the shuffled quiz 
 // Displays questions from randomised category and increments question index for next round
 function displayQ(randomCategory) {
-    if (randomCategory === 0) {
-        console.log(quizCatA[posA]);
-        quizContainer.innerText = quizCatA[posA].question;
-        optionText1.innerText = quizCatA[posA].choice1;
-        optionText2.innerText = quizCatA[posA].choice2;
-        optionText3.innerText = quizCatA[posA].choice3;
-        correctAnswer = (quizCatA[posA].answer);
-        posA++;
-        posTotal++;
-    } else if (randomCategory === 1) {
-        console.log(quizCatB[posB]);
-        quizContainer.innerText = quizCatB[posB].question;
-        optionText1.innerText = quizCatB[posB].choice1;
-        optionText2.innerText = quizCatB[posB].choice2;
-        optionText3.innerText = quizCatB[posB].choice3;
-        correctAnswer = (quizCatB[posB].answer);
-        posB++;
-        posTotal++;
-    } else if (randomCategory === 2) {
-        console.log(quizCatC[posC]);
-        quizContainer.innerText = quizCatC[posC].question;
-        optionText1.innerText = quizCatC[posC].choice1;
-        optionText2.innerText = quizCatC[posC].choice2;
-        optionText3.innerText = quizCatC[posC].choice3;
-        correctAnswer = (quizCatC[posC].answer);
-        posC++;
-        posTotal++;
-    } else {
-        
-    }
-  }  
+  if (randomCategory === 0) {
+    console.log(quizCatA[posA]);
+    quizContainer.innerText = quizCatA[posA].question;
+    optionText1.innerText = quizCatA[posA].choice1;
+    optionText2.innerText = quizCatA[posA].choice2;
+    optionText3.innerText = quizCatA[posA].choice3;
+    correctAnswer = (quizCatA[posA].answer);
+    posA++;
+    posTotal++;
+  } else if (randomCategory === 1) {
+    console.log(quizCatB[posB]);
+    quizContainer.innerText = quizCatB[posB].question;
+    optionText1.innerText = quizCatB[posB].choice1;
+    optionText2.innerText = quizCatB[posB].choice2;
+    optionText3.innerText = quizCatB[posB].choice3;
+    correctAnswer = (quizCatB[posB].answer);
+    posB++;
+    posTotal++;
+  } else if (randomCategory === 2) {
+    console.log(quizCatC[posC]);
+    quizContainer.innerText = quizCatC[posC].question;
+    optionText1.innerText = quizCatC[posC].choice1;
+    optionText2.innerText = quizCatC[posC].choice2;
+    optionText3.innerText = quizCatC[posC].choice3;
+    correctAnswer = (quizCatC[posC].answer);
+    posC++;
+    posTotal++;
+  } else {
+
+  }
+}
 
 //Shuffles quiz array according to Fisher-Yates https://bost.ocks.org/mike/shuffle/
 function shuffle(quiz) {
-  let currentIndex = quiz.length,  randomIndex;
+  let currentIndex = quiz.length,
+    randomIndex;
 
   // While there remain elements to shuffle...
   while (0 !== currentIndex) {
@@ -117,7 +112,8 @@ function shuffle(quiz) {
 
     // And swap it with the current element.
     [quiz[currentIndex], quiz[randomIndex]] = [
-      quiz[randomIndex], quiz[currentIndex]];
+      quiz[randomIndex], quiz[currentIndex]
+    ];
   }
 
   return quiz;
@@ -131,21 +127,21 @@ function checkAnswer(event) {
   incrementStrike();
   checkLength(quiz);
   setTimeout(onOff, 1300);
- }
+}
 
- //get the value of the checked radiobutton to compare to correct answer and increment results
+//get the value of the checked radiobutton to compare to correct answer and increment results
 function compare() {
-let currentAnswer = document.querySelector('input[name="test"]:checked').value;
+  let currentAnswer = document.querySelector('input[name="test"]:checked').value;
   let currentScore = parseInt(resultsContainer.innerHTML);
   let addedScore = parseInt(document.getElementById("value-display").innerHTML);
 
   if (parseInt(currentAnswer) === parseInt(correctAnswer)) {
     resultsContainer.innerText = currentScore + addedScore;
     onOffScore();
-    } else {
-      document.getElementById("incorrect").innerHTML++; 
-    }
-} 
+  } else {
+    document.getElementById("incorrect").innerHTML++;
+  }
+}
 
 //Clears to prep for next question
 function clear() {
@@ -157,10 +153,10 @@ function clear() {
   document.getElementById("c-2").style.color = "white";
   document.getElementById("c-3").style.color = "white";
   document.getElementById("button-start").style.backgroundColor = "orange";
-  
+
 
   // Clears the radiobuttons
-  Array.from( document.querySelectorAll('input[name="test"]:checked'), input => input.checked = false );
+  Array.from(document.querySelectorAll('input[name="test"]:checked'), input => input.checked = false);
 
   //Clear question
   quizContainer.innerText = "QUESTION";
@@ -178,99 +174,90 @@ function incrementStrike() {
   if (incorrect === 1) {
     document.getElementById("strike-1").style.backgroundColor = "red";
     document.getElementById("strike-1").innerHTML = "X";
-    
-} else if (incorrect === 2) {
-  document.getElementById("strike-2").style.backgroundColor = "red";
-  document.getElementById("strike-2").innerHTML = "X";
-   
-} else if (incorrect === 3) {
-  document.getElementById("strike-3").style.backgroundColor = "red";
-  document.getElementById("strike-3").innerHTML = "X";
-  gameoverPopup();
-} 
-};  
-  
+
+  } else if (incorrect === 2) {
+    document.getElementById("strike-2").style.backgroundColor = "red";
+    document.getElementById("strike-2").innerHTML = "X";
+
+  } else if (incorrect === 3) {
+    document.getElementById("strike-3").style.backgroundColor = "red";
+    document.getElementById("strike-3").innerHTML = "X";
+    gameoverPopup();
+  }
+}
+
 // blink "on" state
-function blinkOn()
-{
+function blinkOn() {
   document.getElementById("button-start").style.backgroundColor = "#4d4d4d";
 }
 // blink "off" state
-function blinkOff()
-{
+function blinkOff() {
   document.getElementById("button-start").style.backgroundColor = "orange";
 }
-//Execute blink function every 350 ms to achieve a blink effect end after 4000 ms (less than five seconds)
+//Execute blink function every 350 ms to achieve a blink effect end after 2800 ms
 function onOff() {
- for(let i=700; i < 2800; i=i+700)
- {
-   setTimeout("blinkOn()",i);
-   setTimeout("blinkOff()",i+350);
- }
+  for (let i = 700; i < 2800; i = i + 700) {
+    setTimeout("blinkOn()", i);
+    setTimeout("blinkOff()", i + 350);
+  }
 }
 
+//Executes the checkAnswer function when clicking submit
 let submitAnswer = document.getElementById("button-submit");
 submitAnswer.addEventListener('click', checkAnswer); // left clicks*/
 
-//all OK until here!
-
+// Pop up after 3 strikes that indicated game over, total score and option to play again
 function gameoverPopup() {
-let modal = document.getElementById("modal-gameover");
-let displayScore = document.getElementById("popup");
-// Get the <span> element that closes the modal
-let span = document.getElementsByClassName("close")[0];
+  let modal = document.getElementById("modal-gameover");
+  let displayScore = document.getElementById("popup");
+  // Get the <span> element that closes the modal
+  let span = document.getElementsByClassName("close")[0];
 
-modal.style.display = "block";
-displayScore.innerText = `GAME OVER! Your total score is ${parseInt(resultsContainer.innerHTML)}.`
+  modal.style.display = "block";
+  displayScore.innerText = `GAME OVER! Your total score is ${parseInt(resultsContainer.innerHTML)}.`;
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function () {
+    modal.style.display = "none";
+  }
 }
-
-};
 
 function alldonePopup() {
   let modal = document.getElementById("modal-alldone");
   let displayScore = document.getElementById("popup2");
   // Get the <span> element that closes the modal
   let span = document.getElementsByClassName("close")[1];
-  
+
   modal.style.display = "block";
   displayScore.innerText = `ALL DONE! we have no more questions for you, your total score is ${parseInt(resultsContainer.innerHTML)}. Well done!`
-  
-  // When the user clicks on <span> (x), close the modal
-  span.onclick = function() {
-    modal.style.display = "none";
-  }
-  
-  };
 
-function checkLength() {
-if ((posA >= quizCatA.length) || (posB >= quizCatB.length) || (posC >= quizCatC.length)) {
-  alldonePopup();
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function () {
+    modal.style.display = "none";
+  };
 }
 
+function checkLength() {
+  if ((posA >= quizCatA.length) || (posB >= quizCatB.length) || (posC >= quizCatC.length)) {
+    alldonePopup();
+  }
 }
 
 // blink "on" state
-function blinkOnScore()
-{
+function blinkOnScore() {
   document.getElementById("score-box").style.backgroundColor = "green";
-  
+
 }
 // blink "off" state
-function blinkOffScore()
-{
+function blinkOffScore() {
   document.getElementById("score-box").style.backgroundColor = "grey";
 }
 //Execute blink function every 350 ms to achieve a blink effect end after 4000 ms (less than five seconds)
 function onOffScore() {
- for(let i=600; i < 1200; i=i+600)
- {
-   setTimeout("blinkOnScore()",i);
-   setTimeout("blinkOffScore()",i+300);
- }
+  for (let i = 600; i < 1200; i = i + 600) {
+    setTimeout("blinkOnScore()", i);
+    setTimeout("blinkOffScore()", i + 300);
+  }
 }
 
 /* function screenSize() {
